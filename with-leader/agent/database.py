d5739724fb9539b54c476c5efaefda17f22222e4
Piotr Ulanowski <pioula111@gmail.com>
2022-12-13 12:33:03 +0100

    backend implementation

---
+++ with-leader/agent/database.py
@@ -0,0 +1,21 @@
+import psycopg2
+from config import config
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        return psycopg2.connect(**params)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+def read_query(cur, query, params = ()):
+  cur.execute(query, params)
+  return cur.fetchall()
+
+def write_query(cur, query, params = ()):
+  cur.execute(query, params)
