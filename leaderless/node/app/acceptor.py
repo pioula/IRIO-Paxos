bdccabe6fe0aa569936a8588b453463043b6a9a3
Olaf <olafplacha@gmail.com>
2023-01-10 14:49:07 +0100

    added logging

--- leaderless/node/app/acceptor.py
+++ leaderless/node/app/acceptor.py
@@ -1,7 +1,13 @@
 import pickle
+import logging
 from collections import defaultdict
 
 FILE_NAME = "acceptor.pickle"
+
+logger = logging.getLogger()
+c_handler = logging.StreamHandler()
+logger.addHandler(c_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def create_default_params():
@@ -17,25 +23,35 @@ class Acceptor:
             with open(FILE_NAME, "rb") as infile:
                 on_disk_acceptor = pickle.load(infile)
                 self.parameters = on_disk_acceptor.parameters
+                logger.debug('Instantiated Acceptor object from a file.')
 
         except FileNotFoundError:
             self.parameters = defaultdict(create_default_params)
+            logger.debug('No Acceptor object found on the disk! Creating a new one.')
 
     def handle_prepare(self, run_id, propose_id):
         run_parameters = self.parameters[run_id]
 
         if run_parameters["promised_id"] < propose_id:
+            logger.debug('New propose_id higher than all promised before.')
+
             run_parameters["promised_id"] = propose_id
             return run_parameters["accepted_id"], run_parameters["accepted_val"]
+
+        logger.debug(f'Already promised not smaller ({run_parameters["promised_id"]}) proposal than current propose_id.')
         return None, run_parameters["promised_id"]
 
     def handle_accept(self, run_id, propose_id, val):
         run_parameters = self.parameters[run_id]
 
         if run_parameters["promised_id"] < propose_id:
+            logger.debug('New propose_id higher than all promised before.')
+
             run_parameters["accepted_id"] = propose_id
             run_parameters["accepted_val"] = val
             return True
+
+        logger.debug(f'Already promised not smaller ({run_parameters["promised_id"]}) proposal than current propose_id.')
         return False
 
     def serialize(self):
